c631d2df5c64739d03e656663727ac637ae6f889
matheusribeiro-coder <github.matheusribeiro@gmail.com>
2023-12-06 19:10:25 -0300

    Bibliotecas adicionadas e configurações alteradas

---
+++ tailwind.config.js
@@ -0,0 +1,40 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+    './pages/**/*.{html,js}',
+    './shared/components/**/*.{html,js}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          red: '80080A',
+          blue: '0866B0'
+        },
+        gray: {
+          50: '#F9FAFB',
+          100: '#F3F4F6',
+          200: '#E5E7EB',
+          300: '#D1D5DB',
+          400: '#9CA3AF',
+          500: '#6B7280',
+          600: '#4B5563',
+          700: '#374151',
+          800: '#1F2937',
+          900: '#111827',
+          950: '#030712',
+        },
+        neutral: {
+          white: 'FFFFFF',
+          black: '000000'
+        }
+      }
+    },
+  },
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+  ],
+}
+
